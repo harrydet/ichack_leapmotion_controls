22995ae90b45cc5c191fc45f0df9e8a35ca94893
Harry Detsis <harry.detsis@gmail.com>
2016-02-20 16:31:54 +0000

    Gesture recognition

--- leapmotion_interface.js
+++ leapmotion_interface.js
@@ -33,47 +33,32 @@ function concatJointPosition(id, position) {
 
 //Document ready event
 $( document ).ready(function(){
-    var output = document.getElementById('output');
-    var frameString = "", handString = "", fingerString = "";
-    var hand, finger;
+    //Component initialization
+    $('ul.tabs').tabs();
 
     // Leap.loop uses browser's requestAnimationFrame
     var options = { enableGestures: true };
 
     // Main Leap Loop
     Leap.loop(options, function(frame) {
-        frameString = concatData("frame_id", frame.id);
-        frameString += concatData("num_hands", frame.hands.length);
-        frameString += concatData("num_fingers", frame.fingers.length);
-        frameString += "<br>";
-
-        // Showcase some new V2 features
-        for (var i = 0, len = frame.hands.length; i < len; i++) {
-            hand = frame.hands[i];
-            handString = concatData("hand_type", hand.type);
-            handString += concatData("confidence", hand.confidence);
-            handString += concatData("pinch_strength", hand.pinchStrength);
-            handString += concatData("grab_strength", hand.grabStrength);
-
-            handString += '<br>';
-
-            // Helpers for thumb, pinky, etc.
-            fingerString = concatJointPosition("finger_thumb_dip", hand.thumb.dipPosition);
-            for (var j = 0, len2 = hand.fingers.length; j < len2; j++) {
-                finger = hand.fingers[j];
-                fingerString += concatData("finger_type", finger.type) + " (" + getFingerName(finger.type) + ") <br>";
-                fingerString += concatJointPosition("finger_dip", finger.dipPosition);
-                fingerString += concatJointPosition("finger_pip", finger.pipPosition);
-                fingerString += concatJointPosition("finger_mcp", finger.mcpPosition);
-                fingerString += "<br>";
-            }
-
-            frameString += handString;
-            frameString += fingerString;
+        if(frame.valid && frame.gestures.length > 0){
+            frame.gestures.forEach(function(gesture){
+               switch (gesture.type){
+                   case "circle":
+                       console.log("Circle Gesture");
+                       break;
+                   case "keyTap":
+                       console.log("Key Tap Gesture");
+                       break;
+                   case "screenTap":
+                       console.log("Screen Tap Gesture");
+                       break;
+                   case "swipe":
+                       console.log("Swipe Gesture");
+                       break;
+               }
+            });
         }
-
-        output.innerHTML = frameString;
-
     });
 
 });
